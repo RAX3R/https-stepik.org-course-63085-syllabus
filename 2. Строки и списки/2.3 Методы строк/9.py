d8442d50f370ea886796ade7a536169a4d065c54
RAX3R <17927942+RAX3R@users.noreply.github.com>
2023-04-25 20:38:19 +0300

    2.3 - 9 (update)

--- 2. Строки и списки/2.3 Методы строк/9.py
+++ 2. Строки и списки/2.3 Методы строк/9.py
@@ -1,11 +1,1 @@
-s = input().lower()
-
-s = s.replace('a', '')
-s = s.replace('o', '')
-s = s.replace('y', '')
-s = s.replace('e', '')
-s = s.replace('u', '')
-s = s.replace('i', '')
-s = s.replace('', '.')
-
-print(s[:-1])
+print(input().rfind('a'))
